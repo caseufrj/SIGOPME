ef5c0d8cc8fc5b92ac3a660cc417e41a1be0999d
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-08 11:01:50 -0300

    Refactor initialize_database method formatting

--- src/database/database_service.py
+++ src/database/database_service.py
@@ -11,27 +11,27 @@ class DatabaseService:
         return sqlite3.connect(cls.DB_PATH)
 
     @classmethod
-def initialize_database(cls):
-
-    conn = cls.get_connection()
-
-    arquivos = [
-        "database/create_database.sql",
-        "database/views.sql",
-        "database/seed_data.sql"
-    ]
-
-    for arquivo in arquivos:
-
-        with open(
-            arquivo,
-            "r",
-            encoding="utf-8"
-        ) as f:
-
-            conn.executescript(f.read())
-
-    conn.commit()
-    conn.close()
-
-    print("Banco inicializado com sucesso.")
+    def initialize_database(cls):
+    
+        conn = cls.get_connection()
+    
+        arquivos = [
+            "database/create_database.sql",
+            "database/views.sql",
+            "database/seed_data.sql"
+        ]
+    
+        for arquivo in arquivos:
+    
+            with open(
+                arquivo,
+                "r",
+                encoding="utf-8"
+            ) as f:
+    
+                conn.executescript(f.read())
+    
+        conn.commit()
+        conn.close()
+    
+        print("Banco inicializado com sucesso.")
