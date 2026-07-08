df41a0a9bb7d9377ecad88b37783943ad0826f44
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-08 15:22:10 -0300

    Update dashboard_frame.py

--- src/views/frames/dashboard_frame.py
+++ src/views/frames/dashboard_frame.py
@@ -1,1 +1,15 @@
+import tkinter as tk
 
+
+class DashboardFrame(tk.Frame):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        titulo = tk.Label(
+            self,
+            text="Dashboard",
+            font=("Arial", 18, "bold")
+        )
+
+        titulo.pack(pady=20)
