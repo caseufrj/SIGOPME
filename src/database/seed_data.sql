3db2bddfb15a52697599a841baa39cd4bb17a097
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-08 09:15:39 -0300

    Create seed_data.sql

---
+++ src/database/seed_data.sql
@@ -0,0 +1,15 @@
+INSERT OR IGNORE INTO TiposMovimentacao (Id, Nome)
+VALUES (1, 'ENTRADA_CONSIGNACAO');
+
+INSERT OR IGNORE INTO TiposMovimentacao (Id, Nome)
+VALUES (2, 'ENTRADA_VENDA');
+
+INSERT OR IGNORE INTO TiposMovimentacao (Id, Nome)
+VALUES (3, 'RETIRADO');
+
+INSERT OR IGNORE INTO TiposMovimentacao (Id, Nome)
+VALUES (4, 'UTILIZADO');
+
+INSERT OR IGNORE INTO TiposMovimentacao (Id, Nome)
+VALUES (5,Perfeito. Vamos criar o banco **de verdade** agora.
+
