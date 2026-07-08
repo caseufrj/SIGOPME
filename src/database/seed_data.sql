cdcf42f357d1173301e5613a514baf68f8dcbcbc
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-08 09:16:42 -0300

    Change INSERT statements to omit IDs in seed data

--- src/database/seed_data.sql
+++ src/database/seed_data.sql
@@ -1,15 +1,20 @@
-INSERT OR IGNORE INTO TiposMovimentacao (Id, Nome)
-VALUES (1, 'ENTRADA_CONSIGNACAO');
+INSERT INTO TiposMovimentacao (Nome)
+VALUES ('ENTRADA_CONSIGNACAO');
 
-INSERT OR IGNORE INTO TiposMovimentacao (Id, Nome)
-VALUES (2, 'ENTRADA_VENDA');
+INSERT INTO TiposMovimentacao (Nome)
+VALUES ('ENTRADA_VENDA');
 
-INSERT OR IGNORE INTO TiposMovimentacao (Id, Nome)
-VALUES (3, 'RETIRADO');
+INSERT INTO TiposMovimentacao (Nome)
+VALUES ('RETIRADO');
 
-INSERT OR IGNORE INTO TiposMovimentacao (Id, Nome)
-VALUES (4, 'UTILIZADO');
+INSERT INTO TiposMovimentacao (Nome)
+VALUES ('UTILIZADO');
 
-INSERT OR IGNORE INTO TiposMovimentacao (Id, Nome)
-VALUES (5,Perfeito. Vamos criar o banco **de verdade** agora.
+INSERT INTO TiposMovimentacao (Nome)
+VALUES ('DEVOLVIDO');
 
+INSERT INTO TiposMovimentacao (Nome)
+VALUES ('EXTRAVIADO');
+
+INSERT INTO TiposMovimentacao (Nome)
+VALUES ('PAGO');
