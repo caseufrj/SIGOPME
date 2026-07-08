5ba550d0247e4508f3113cb2308a1de6711f8418
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-08 15:23:15 -0300

    Create movimentacoes_frame.py

---
+++ src/views/frames/movimentacoes_frame.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+
+class MovimentacoesFrame(tk.Frame):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        titulo = tk.Label(
+            self,
+            text="Movimentações",
+            font=("Arial", 18, "bold")
+        )
+
+        titulo.pack(pady=20)
