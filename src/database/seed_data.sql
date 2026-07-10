76fa47e28256efabe5b57104ea6f72bf27a8ab3b
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-10 09:33:55 -0300

    Update seed_data.sql

--- src/database/seed_data.sql
+++ src/database/seed_data.sql
@@ -1,22 +1,22 @@
-INSERT INTO TiposMovimentacao (Nome)
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
 VALUES ('ENTRADA_CONSIGNACAO');
 
-INSERT INTO TiposMovimentacao (Nome)
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
 VALUES ('ENTRADA_VENDA');
 
-INSERT INTO TiposMovimentacao (Nome)
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
 VALUES ('RETIRADO');
 
-INSERT INTO TiposMovimentacao (Nome)
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
 VALUES ('UTILIZADO');
 
-INSERT INTO TiposMovimentacao (Nome)
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
 VALUES ('DEVOLVIDO');
 
-INSERT INTO TiposMovimentacao (Nome)
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
 VALUES ('EXTRAVIADO');
 
-INSERT INTO TiposMovimentacao (Nome)
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
 VALUES ('PAGO');
 
 INSERT OR IGNORE INTO Usuarios (
