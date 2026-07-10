9d92016bd20c83e1855e5d5db71ec85829644bd0
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-10 12:24:44 -0300

    Update entrada_service.py

--- src/services/entrada_service.py
+++ src/services/entrada_service.py
@@ -80,3 +80,100 @@ class EntradaService:
 
         conn.commit()
         conn.close()
+
+    @staticmethod
+    def obter_por_id(id_registro):
+
+        conn = DatabaseService.get_connection()
+
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            SELECT
+                Id,
+                NumeroNF,
+                SerieNF,
+                DataEmissao,
+                DataEntrada,
+                TipoEntrada,
+                Fornecedor,
+                CodItem,
+                NomeMaterial,
+                Quantidade,
+                Observacao
+            FROM Entradas
+            WHERE Id = ?
+        """, (id_registro,))
+
+        resultado = cursor.fetchone()
+
+        conn.close()
+
+        return resultado
+
+    @staticmethod
+    def atualizar(
+        id_registro,
+        numero_nf,
+        serie_nf,
+        data_emissao,
+        data_entrada,
+        tipo_entrada,
+        fornecedor,
+        codigo_item,
+        nome_material,
+        quantidade,
+        observacao
+    ):
+
+        conn = DatabaseService.get_connection()
+
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            UPDATE Entradas
+            SET
+                NumeroNF = ?,
+                SerieNF = ?,
+                DataEmissao = ?,
+                DataEntrada = ?,
+                TipoEntrada = ?,
+                Fornecedor = ?,
+                CodItem = ?,
+                NomeMaterial = ?,
+                Quantidade = ?,
+                Observacao = ?
+            WHERE Id = ?
+        """, (
+            numero_nf,
+            serie_nf,
+            data_emissao,
+            data_entrada,
+            tipo_entrada,
+            fornecedor,
+            codigo_item,
+            nome_material,
+            quantidade,
+            observacao,
+            id_registro
+        ))
+
+        conn.commit()
+
+        conn.close()
+
+    @staticmethod
+    def excluir(id_registro):
+
+        conn = DatabaseService.get_connection()
+
+        cursor = conn.cursor()
+
+        cursor.execute("""
+            DELETE FROM Entradas
+            WHERE Id = ?
+        """, (id_registro,))
+
+        conn.commit()
+
+        conn.close()
