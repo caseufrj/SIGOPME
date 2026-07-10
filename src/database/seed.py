7340c239ef02c51937dfc088fc9800586fb0f73a
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-10 10:14:42 -0300

    Create seed.py

---
+++ src/database/seed.py
@@ -0,0 +1,48 @@
+SEED_SQL = """
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
+VALUES ('ENTRADA_CONSIGNACAO');
+
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
+VALUES ('ENTRADA_VENDA');
+
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
+VALUES ('RETIRADO');
+
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
+VALUES ('UTILIZADO');
+
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
+VALUES ('DEVOLVIDO');
+
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
+VALUES ('EXTRAVIADO');
+
+INSERT OR IGNORE INTO TiposMovimentacao (Nome)
+VALUES ('PAGO');
+
+INSERT OR IGNORE INTO Usuarios (
+    Id,
+    Nome,
+    Login,
+    SenhaHash,
+    Perfil
+)
+VALUES (
+    1,
+    'Administrador',
+    'admin',
+    'admin',
+    'ADMIN'
+);
+
+INSERT OR IGNORE INTO Configuracoes (
+    Id,
+    NomeHospital,
+    VersaoSistema
+)
+VALUES (
+    1,
+    'HU-CAS/UFRJ',
+    '0.1'
+);
+"""
