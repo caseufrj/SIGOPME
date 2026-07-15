59431993e72f0adb23a36a82dab4c924db7eed31
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-15 07:13:33 -0300

    Update paciente_service.py

--- src/services/paciente_service.py
+++ src/services/paciente_service.py
@@ -4,7 +4,7 @@ from database.database_service import DatabaseService
 class PacienteService:
 
     @staticmethod
-    def listar_todos():
+    def obter_por_registro(registro):
 
         conn = DatabaseService.get_connection()
 
@@ -14,21 +14,19 @@ class PacienteService:
             SELECT
                 Id,
                 Registro,
-                Nome,
-                DataNascimento
+                Nome
             FROM Pacientes
-            WHERE Ativo = 1
-            ORDER BY Nome
-        """)
+            WHERE Registro = ?
+        """, (registro,))
 
-        resultado = cursor.fetchall()
+        resultado = cursor.fetchone()
 
         conn.close()
 
         return resultado
 
     @staticmethod
-    def pesquisar(texto):
+    def obter_por_nome(nome):
 
         conn = DatabaseService.get_connection()
 
@@ -38,43 +36,10 @@ class PacienteService:
             SELECT
                 Id,
                 Registro,
-                Nome,
-                DataNascimento
+                Nome
             FROM Pacientes
-            WHERE
-                Ativo = 1
-                AND (
-                    UPPER(Registro) LIKE UPPER(?)
-                    OR UPPER(Nome) LIKE UPPER(?)
-                )
-            ORDER BY Nome
-        """, (
-            f"%{texto}%",
-            f"%{texto}%"
-        ))
-
-        resultado = cursor.fetchall()
-
-        conn.close()
-
-        return resultado
-
-    @staticmethod
-    def obter_por_id(id_registro):
-
-        conn = DatabaseService.get_connection()
-
-        cursor = conn.cursor()
-
-        cursor.execute("""
-            SELECT
-                Id,
-                Registro,
-                Nome,
-                DataNascimento
-            FROM Pacientes
-            WHERE Id = ?
-        """, (id_registro,))
+            WHERE Nome = ?
+        """, (nome,))
 
         resultado = cursor.fetchone()
 
@@ -85,8 +50,7 @@ class PacienteService:
     @staticmethod
     def inserir(
         registro,
-        nome,
-        data_nascimento
+        nome
     ):
 
         conn = DatabaseService.get_connection()
@@ -96,62 +60,13 @@ class PacienteService:
         cursor.execute("""
             INSERT INTO Pacientes (
                 Registro,
-                Nome,
-                DataNascimento
+                Nome
             )
-            VALUES (?, ?, ?)
+            VALUES (?, ?)
         """, (
             registro,
-            nome,
-            data_nascimento
+            nome
         ))
-
-        conn.commit()
-
-        conn.close()
-
-    @staticmethod
-    def atualizar(
-        id_registro,
-        registro,
-        nome,
-        data_nascimento
-    ):
-
-        conn = DatabaseService.get_connection()
-
-        cursor = conn.cursor()
-
-        cursor.execute("""
-            UPDATE Pacientes
-            SET
-                Registro = ?,
-                Nome = ?,
-                DataNascimento = ?
-            WHERE Id = ?
-        """, (
-            registro,
-            nome,
-            data_nascimento,
-            id_registro
-        ))
-
-        conn.commit()
-
-        conn.close()
-
-    @staticmethod
-    def excluir(id_registro):
-
-        conn = DatabaseService.get_connection()
-
-        cursor = conn.cursor()
-
-        cursor.execute("""
-            UPDATE Pacientes
-            SET Ativo = 0
-            WHERE Id = ?
-        """, (id_registro,))
 
         conn.commit()
 
