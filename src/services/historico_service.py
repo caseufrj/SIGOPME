a8d58d0edfcd3ae90651d319885c4f5a1033db43
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-15 09:23:08 -0300

    Update historico_service.py

--- src/services/historico_service.py
+++ src/services/historico_service.py
@@ -5,19 +5,35 @@ class HistoricoService:
 
     @staticmethod
     def registrar(
-        estoque_id,
-        numero_licitacao,
-        cod_item,
-        nome_material,
-        lote,
-        codigo_unico,
-        status_anterior,
-        status_novo,
+
+        estoque_id=None,
+
+        tipo=None,
+        acao=None,
+
+        numero_licitacao=None,
+
+        fornecedor=None,
+        documento=None,
+
+        cod_item=None,
+        nome_material=None,
+
+        lote=None,
+        codigo_unico=None,
+
+        status_anterior=None,
+        status_novo=None,
+
         paciente_nome=None,
         paciente_registro=None,
+
         sala=None,
+
         usuario="Sistema",
+
         observacao=""
+
     ):
 
         conn = DatabaseService.get_connection()
@@ -25,45 +41,103 @@ class HistoricoService:
         cursor = conn.cursor()
 
         cursor.execute("""
+
             INSERT INTO HistoricoMovimentacao (
 
                 EstoqueId,
+
+                Tipo,
+                Acao,
+
                 NumeroLicitacao,
+
+                Fornecedor,
+                Documento,
+
                 CodItem,
                 NomeMaterial,
+
                 Lote,
                 CodigoUnico,
+
                 StatusAnterior,
                 StatusNovo,
+
                 PacienteNome,
                 PacienteRegistro,
+
                 Sala,
+
                 Usuario,
+
                 DataMovimento,
+
                 Observacao
 
             )
+
             VALUES (
 
-                ?, ?, ?, ?, ?, ?, ?, ?,
-                ?, ?, ?, ?, datetime('now'),
+                ?,
+
+                ?,
+                ?,
+
+                ?,
+
+                ?,
+                ?,
+
+                ?,
+                ?,
+
+                ?,
+                ?,
+
+                ?,
+                ?,
+
+                ?,
+                ?,
+
+                ?,
+
+                ?,
+
+                datetime('now'),
+
                 ?
 
             )
+
         """, (
 
             estoque_id,
+
+            tipo,
+            acao,
+
             numero_licitacao,
+
+            fornecedor,
+            documento,
+
             cod_item,
             nome_material,
+
             lote,
             codigo_unico,
+
             status_anterior,
             status_novo,
+
             paciente_nome,
             paciente_registro,
+
             sala,
+
             usuario,
+
             observacao
 
         ))
