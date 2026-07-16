00412c442cada2d1a6ccf937995a064be9c1467c
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-16 15:04:15 -0300

    Update nota_itens_service.py

--- src/services/nota_itens_service.py
+++ src/services/nota_itens_service.py
@@ -52,3 +52,36 @@ class NotaItensService:
         conn.commit()
 
         conn.close()
+
+    @staticmethod
+    def listar_por_entrada(entrada_id):
+    
+        conn = DatabaseService.get_connection()
+    
+        cursor = conn.cursor()
+    
+        cursor.execute("""
+            SELECT
+    
+                CodItem,
+                NomeItem,
+                Lote,
+                SerieProduto,
+                DataValidade,
+                Quantidade,
+                ValorUnitario,
+                ValorTotal
+    
+            FROM NotaItens
+    
+            WHERE EntradaId = ?
+    
+            ORDER BY NomeItem
+    
+        """, (entrada_id,))
+    
+        dados = cursor.fetchall()
+    
+        conn.close()
+    
+        return dados
