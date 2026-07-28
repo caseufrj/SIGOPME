7ed361180b45ef15b5d0cc326122af508012d6a0
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-28 13:12:42 -0300

    Update solicitacao_itens_service.py

--- src/services/solicitacao_itens_service.py
+++ src/services/solicitacao_itens_service.py
@@ -63,6 +63,7 @@ class SolicitacaoItensService:
                 Id,
                 CodItem,
                 NomeItem,
+                Lote,
                 Quantidade,
                 QuantidadeAtendida,
                 Status
