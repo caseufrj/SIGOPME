7503eee79b814b750d1fb57e1d5cf66a3d673f95
caseufrj <45002307+caseufrj@users.noreply.github.com>
2026-07-30 12:02:37 -0300

    Update protocolo_rastreabilidade_service.py

--- src/services/protocolo_rastreabilidade_service.py
+++ src/services/protocolo_rastreabilidade_service.py
@@ -27,52 +27,61 @@ class ProtocoloRastreabilidadeService:
 
     @staticmethod
     def obter_protocolo_completo(protocolo):
-
+    
         conn = DatabaseService.get_connection()
+    
         cursor = conn.cursor()
-
+    
         cursor.execute("""
-            cursor.execute("""
-                SELECT
-            
-                    s.Id,
-                    s.PacienteRegistro,
-                    s.PacienteNome,
-            
-                    si.CodItem,
-                    si.NomeItem,
-                    si.Lote,
-                    si.Status,
-            
-                    s.DataSolicitacao
-            
-                FROM Solicitacoes s
-            
-                INNER JOIN SolicitacaoItens si
-                    ON si.SolicitacaoId = s.Id
-            
-                WHERE s.Id = ?
-            
-                LIMIT 1
-            
-            """, (protocolo,))
-
+            SELECT
+    
+                s.Id,
+                s.PacienteRegistro,
+                s.PacienteNome,
+    
+                si.CodItem,
+                si.NomeItem,
+                si.Lote,
+                si.Status,
+    
+                s.DataSolicitacao
+    
+            FROM Solicitacoes s
+    
+            INNER JOIN SolicitacaoItens si
+                ON si.SolicitacaoId = s.Id
+    
+            WHERE s.Id = ?
+    
+            LIMIT 1
+    
+        """, (protocolo,))
+    
         resultado = cursor.fetchone()
-
+    
         print(resultado)
-
+    
         conn.close()
-
+    
         if not resultado:
             return None
-
+    
         return ProtocoloRastreabilidade(
+    
             protocolo=resultado[0],
+    
             registro=resultado[1] or "",
+    
             paciente=resultado[2] or "",
+    
             codigo_item=resultado[3] or "",
+    
             nome_material=resultado[4] or "",
+    
             lote=resultado[5] or "",
+    
             status=resultado[6] or "",
+    
             data_retirada=resultado[7]
+    
         )
